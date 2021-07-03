4d366cc2da2809c87c4f832c91c994631adbbfb2
GeorgeGithiri5 <georgegithiri002@gmail.com>
2021-07-03 20:05:20 +0300

    React-Node

---
+++ go.mod
@@ -0,0 +1,10 @@
+module test
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gofiber/fiber/v2 v2.14.0 // indirect
+	gorm.io/driver/mysql v1.1.1 // indirect
+	gorm.io/gorm v1.21.11 // indirect
+)
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/react_go?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":8000")
+}
